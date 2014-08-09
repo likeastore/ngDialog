3697d71ac35ef1f15b79639c8557941be9669402
Robert Mulder <mulder@creativeid.nl>
2014-08-09 16:53:05 +0200

    Since commit e27e6bc6f157704c66ac9020a54ed6a3b96f9c91 the forceBodyReload is no longer a property of defaults.

--- js/ngDialog.js
+++ js/ngDialog.js
@@ -35,7 +35,7 @@
 		this.$get = ['$document', '$templateCache', '$compile', '$q', '$http', '$rootScope', '$timeout', '$window',
 			function ($document, $templateCache, $compile, $q, $http, $rootScope, $timeout, $window) {
 				var $body = $document.find('body');
-				if (defaults.forceBodyReload) {
+				if (forceBodyReload) {
 					$rootScope.$on('$locationChangeSuccess', function () {
 						$body = $document.find('body');
 					});
