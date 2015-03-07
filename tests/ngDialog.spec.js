362c4e34f887dc9d0c3c919348c080e6b67c049a
Ruaidhri Devery <ruaidhri@zymeworks.com>
2015-03-06 16:05:27 -0800

    added some simple tests for template compilation

--- tests/ngDialog.spec.js
+++ tests/ngDialog.spec.js
@@ -4,6 +4,20 @@ describe("ngDialog", function () {
 
   beforeEach(module('ngDialog'));
 
+  afterEach(inject(function (ngDialog, $document) {
+    ngDialog.closeAll();
+    [].slice.call(
+      $document.find('body').children()
+    )
+    .map(angular.element)
+    .forEach(function (elm) {
+      if (elm.hasClass('ngdialog')) {
+        // yuck
+        elm.triggerHandler('animationend');
+      }
+    });
+  }));
+
   it('should inject the ngDialog service', inject(function(ngDialog) {
     expect(ngDialog).toBeDefined();
   }));
@@ -33,4 +47,36 @@ describe("ngDialog", function () {
     });
   });
 
+  describe("with a plain template", function () {
+    var elm
+    beforeEach(inject(function (ngDialog, $timeout, $document, $rootScope) {
+      var id = ngDialog.open({
+        template: "<div><p>some text {{1 + 1}}</p></div>",
+        plain: true
+      }).id;
+      $timeout.flush();
+      elm = $document[0].getElementById(id);
+    }));
+
+    it('should have compiled the html', inject(function($document) {
+      expect(elm.textContent).toEqual('some text 2');
+    }));
+  });
+
+  describe("with a plain template URL", function () {
+    var elm;
+    beforeEach(inject(function (ngDialog, $timeout, $document, $rootScope, $httpBackend) {
+      $httpBackend.whenGET('test.html').respond("<div><p>some text {{1 + 1}}</p></div>");
+      var id = ngDialog.open({
+        templateUrl: "test.html"
+      }).id;
+      $httpBackend.flush();
+      $timeout.flush();
+      elm = $document[0].getElementById(id);
+    }));
+
+    it('should have compiled the html', inject(function($document) {
+      expect(elm.textContent).toEqual('some text 2');
+    }));
+  });
 });
