333cd19df98c4d78f28f3f24e2b7e976e0b24191
Michael Robinson <michael.robinson@solnet.co.nz>
2015-09-19 19:50:09 +1200

    Initial go at porting code from https://github.com/Tathanen/ngDialog/tree/ngDialog-test to main repo

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,8 +1,8 @@
 module.exports.config = {
   allScriptsTimeout: 11000,
-  specs: ['tests/protractor/test.js'],
+  specs: ['tests/protractor/**/*.js'],
   multiCapabilities: [{
-    'browserName': 'chrome'
+    'browserName': 'firefox'
   }],
   framework: 'jasmine',
   jasmineNodeOpts: {
@@ -12,16 +12,16 @@ module.exports.config = {
     dir: 'mocks',
     default: []
   },
-  seleniumAddress: 'http://chromestandalone:4444/wd/hub',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
   plugins: [{
       path: 'node_modules/protractor/plugins/accessibility',
       chromeA11YDevTools: {
-        treatWarningsAsFailures: false
+        treatWarningsAsFailures: true
       }
     },
     {
       path: 'node_modules/protractor/plugins/console',
       failOnWarning: false,
-      failOnError: true
+      failOnError: false
     }]
 };
