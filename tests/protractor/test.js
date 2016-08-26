ca0cb76ff16e0ecaf1a3ac3c60bf492702867414
Michael Robinson <mike@pagesofinterest.net>
2016-08-26 14:27:22 +1200

    Made protractor tests more specific

--- tests/protractor/test.js
+++ tests/protractor/test.js
@@ -4,25 +4,25 @@ describe('ngDialog', function() {
   });
 
   it('should add padding to the body tag when a scrollbar is present', function() {
-    expect( $('body').getCssValue('padding-right') ).toBe( "0px" );
+    expect( $('body').getCssValue('padding-right') ).toBe( '0px' );
     $('.openPaddingLink').click();
     browser.sleep( 500 );
-    expect( $('body').getCssValue('padding-right') ).not.toBe( "0px" );
+    expect(element(by.css('body')).getCssValue('padding-right')).not.toMatch(/[0-9]{,2}px/);
     $('.ngdialog-overlay').click();
     browser.sleep( 500 );
-    expect( $('body').getCssValue('padding-right') ).toBe( "0px" );
+    expect(element(by.css('body')).getCssValue('padding-right')).toBe( '0px' );
   });
 
   it('should communicate the body padding to the application', function() {
-    expect( $('body').getCssValue('padding-right') ).toBe( "0px" );
-    expect( $('body').getCssValue('padding-right') ).toBe( $('.paddingHeader').getCssValue('padding-right') );
+    expect(element(by.css('body')).getCssValue('padding-right')).toBe( '0px' );
+    expect(element(by.css('body')).getCssValue('padding-right')).toBe( element(by.css('.paddingHeader')).getCssValue('padding-right') );
     $('.openPaddingLink').click();
     browser.sleep( 500 );
-    expect( $('body').getCssValue('padding-right') ).not.toBe( "0px" );
-    expect( $('body').getCssValue('padding-right') ).toBe( $('.paddingHeader').getCssValue('padding-right') );
+    expect(element(by.css('body')).getCssValue('padding-right')).not.toMatch(/[0-9]{,2}px/);
+    expect(element(by.css('body')).getCssValue('padding-right')).toBe( element(by.css('.paddingHeader')).getCssValue('padding-right') );
     $('.ngdialog-overlay').click();
     browser.sleep( 500 );
-    expect( $('body').getCssValue('padding-right') ).toBe( "0px" );
-    expect( $('body').getCssValue('padding-right') ).toBe( $('.paddingHeader').getCssValue('padding-right') );
+    expect(element(by.css('body')).getCssValue('padding-right')).toBe( '0px' );
+    expect(element(by.css('body')).getCssValue('padding-right')).toBe( element(by.css('.paddingHeader')).getCssValue('padding-right') );
   });
 });
